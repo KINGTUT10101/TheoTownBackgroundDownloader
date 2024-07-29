99a83d3db6b39858499b702e5bf9300654ac4dbf
KINGTUT10101 <kingtut10101@gmail.com>
2024-07-29 13:25:18 -0500

    Add better error messages and input params

--- backgroundDownloader.py
+++ backgroundDownloader.py
@@ -1,6 +1,14 @@
 import urllib.request
 import re
 import os
+import sys
+
+# Get input parameters\
+outputDir = None
+try:
+    outputDir = os.path.abspath (sys.argv[1])
+except:
+    raise Exception ("OUTPUT DIRECTORY IS NOT DEFINED IN INPUT PARAMETERS")
 
 urlRegex = r'(?:href)\s*=\s*"(https:.+data.theotown.com.+get_file.php\?\S+)"\s+'
 
@@ -20,7 +28,10 @@ def downloadImage (link, filename):
 if __name__ == '__main__':
     # Get page html
     print ("Getting page contents...")
-    pageContents = urllib.request.urlopen("https://forum.theotown.com/gallery/").read ().decode ("utf8")
+    try:
+        pageContents = urllib.request.urlopen("https://forum.theotown.com/gallery/").read ().decode ("utf8")
+    except:
+        raise Exception ("GALLERY LINK MAY BE BROKEN. CONTACT DEVELOPER OF THIS SCRIPT FOR HELP")
 
     # Extract links from HTML data
     print ("Extracting image links...")
@@ -28,7 +39,12 @@ if __name__ == '__main__':
 
     # Save images to disk
     print ("Saving images to disk...")
-    for index, link in enumerate (urlList):
-        print (str (index + 1) + "/" + str (len (urlList)) + ": " + link)
-        # TODO: make sure folder path exists before doing this
-        downloadImage (link, os.path.join ("C:", os.sep, "Users", os.getlogin (), "Pictures", "TheoTown Gallery", "popular_background" + str (index + 1) + ".png"))
+    try:
+        for index, link in enumerate (urlList):
+            print (str (index + 1) + "/" + str (len (urlList)) + ": " + link)
+            # TODO: make sure folder path exists before doing this
+            # downloadImage (link, os.path.join ("C:", os.sep, "Users", os.getlogin (), "Pictures", "TheoTown Gallery", "popular_background" + str (index + 1) + ".png"))
+            downloadImage (link, outputDir)
+
+    except:
+        raise Exception ("ENSURE THAT THE DIRECTORY IS VALID AND NOT HIDDEN BY THE FILESYSTEM")
